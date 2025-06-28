08c3f0888adc42db3ad5d07ce2d8856ad9643b92
Manuel Rascon <mrascon19@cnm.edu>
2025-06-28 13:47:45 -0600

    created projects directory structure and added fist global css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Contact from "./pages/contact/Contact"
+import Portfolio from "./pages/portfolio/Portfolio"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import AppLayout from "./components/AppLayout"
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: '/', 
+      element: <AppLayout />,
+      children: [
+        {
+          path: '/', 
+          element: <Home />,
+        },
+        {
+          path: '/about', 
+          element: <About />,
+        },
+        {
+          path: '/portfolio', 
+          element: <Portfolio />,
+        },
+        {
+          path: '/contact', 
+          element: <Contact />,
+        },
+      ]
+    }
+  ])
+  return <RouterProvider router={router} />;
 }
 
 export default App 
