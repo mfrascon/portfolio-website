e9bf97b8a96494afb353d0871f27d94d01dfbd64
mfrascon <mfrascon.g@gmail.com>
2025-07-07 19:53:54 -0600

    added styling and picture to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,33 @@
+import Profile from '../../assets/couple-pic.jpg'
+import { Link } from 'react-router';
+import { RiArrowRightLine } from "react-icons/ri";
+import './home.css';
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home-container grid">
+      <img src={Profile} alt='' className='home-img' />
+
+      <div className='home-content'>
+        <h1 className='home-title'>
+          <span>My Name Is Manuel Rascon</span> <br /> Jr. Full-Stack Web Developer
+        </h1>
+
+        <p className='home-description'>
+          Hi, I'm a Full-Stack Web Developer passionate about building modern, user-friendly websites. After completing an intensive full-stack web development bootcamp, I have been applying what I learned by creating personal projects that showcase my skills across both front-end and back-end technologies. Each project is an opportunity to grow, experiment, and build something awesome — all while gaining hands-on experience in real-world development.
+        </p>
+
+        <Link to='/about' className='button'>
+          Check Out My Projects
+          <span className='button-icon'>
+            <RiArrowRightLine />
+          </span>
+        </Link>
+      </div>
+      <div className='color-block'>
+
+      </div>
+    </section>
   )
 }
 
